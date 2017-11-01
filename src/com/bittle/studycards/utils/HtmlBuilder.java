94661833c2b8bb23b4b2d7a3c111abfdf7377474
oscar0812 <oscar0812torres@gmail.com>
2017-11-01 12:35:20 -0500

    center divs fixed, and appendRightToLeft and appendLeftToRight methods added.

--- src/com/bittle/studycards/utils/HtmlBuilder.java
+++ src/com/bittle/studycards/utils/HtmlBuilder.java
@@ -17,24 +17,41 @@ class HtmlBuilder {
     HtmlBuilder(double boxHeight, boolean doCenter, double fontSize) {
         // to put divs side by side with least space
         //double width = 49.43535;
-        double width = 49.71;
+        double width = 50;
         String border = "border: 1px solid black;";
-        String center = "text-align: center;";
-        String font = "font-size: "+fontSize+"pt;";
+        String center1 = "text-align: center; display: table;";
+        String center2 = "display: table-cell; vertical-align: middle;";
+        String font = "font-size: " + fontSize + "pt;";
 
         newRow = "<tr>\n" +
                 "<div style=\"width: " + width + "%; height: " + boxHeight + "px; float:left;" + border +
-                "" + (doCenter ? center : "") +font+ "\">" + leftCard + "</div>\n" +
+                (doCenter ? center1 : "") + font + "\"><div style=\"" + (doCenter ? center2 : "") + "\">" +
+                leftCard + "</div></div>\n" +
+
                 "<div style=\"width: " + width + "%; height: " + boxHeight + "px; float:right;" + border +
-                "" + (doCenter ? center : "") + font+"\">" + rightCard + "</div></tr>\n";
+                (doCenter ? center1 : "") + font + "\"><div style=\"" + (doCenter ? center2 : "") + "\">" +
+                rightCard + "</div></div></tr>\n";
     }
 
-    void append(String entry) {
+    // append on the first column first, how questions are shown
+    void appendLeftToRight(String entry) {
         if (currentRow.isEmpty()) {
             currentRow = newRow;
             currentRow = currentRow.replace(leftCard, entry);
         } else {
             currentRow = currentRow.replace(rightCard, entry);
+            builder.append(currentRow);
+            currentRow = "";
+        }
+    }
+
+    // append the second column first, how answers are shown (since pages are flipped this way when printed)
+    void appendRightToLeft(String entry) {
+        if (currentRow.isEmpty()) {
+            currentRow = newRow;
+            currentRow = currentRow.replace(rightCard, entry);
+        } else {
+            currentRow = currentRow.replace(leftCard, entry);
             builder.append(currentRow);
             currentRow = "";
         }
