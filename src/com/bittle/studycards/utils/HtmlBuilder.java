9eeca0316c8450644167925fc0dcb528a07c3859
oscar0812 <oscar0812torres@gmail.com>
2017-11-01 23:45:33 -0500

    helper methods to append divs, and respect decision if font is set

--- src/com/bittle/studycards/utils/HtmlBuilder.java
+++ src/com/bittle/studycards/utils/HtmlBuilder.java
@@ -13,16 +13,18 @@ class HtmlBuilder {
 
     // for calculating font size
     private double boxHeight;
+    private double fontSize = -1;
 
     HtmlBuilder(double boxHeight, boolean doCenter, double fontSize) {
         this.boxHeight = boxHeight;
+        this.fontSize = fontSize;
         // to put divs side by side with least space
         //double width = 49.43535;
         double width = 50;
         String border = "border: 1px solid black;";
         String center1 = "text-align: center; display: table;";
         String center2 = "display: table-cell; vertical-align: middle;";
-        String font = "font-size: " + fontSize + "pt;";
+        String font = "font-size: " + fontSize + "px;";
 
         String nr = "<tr>\n" +
                 "<div style=\"width: " + width + "%; height: " + boxHeight + "px; float:left;" + border +
@@ -39,10 +41,10 @@ class HtmlBuilder {
     // append on the first column first, how questions are shown
     void appendLeftToRight(String entry) {
         if (currentRow == 0) {
-            newRow.select(".left").first().text(entry);
+            setLeft(entry);
             currentRow++;
         } else {
-            newRow.select(".right").first().text(entry);
+            setRight(entry);
             mainHtml.select("body").first().append(newRow.html());
             currentRow = 0;
         }
@@ -52,15 +54,46 @@ class HtmlBuilder {
     // append the second column first, how answers are shown (since pages are flipped this way when printed)
     void appendRightToLeft(String entry) {
         if (currentRow == 0) {
-            newRow.select(".right").first().text(entry);
+            setRight(entry);
             currentRow++;
         } else {
-            newRow.select(".left").first().text(entry);
+            setLeft(entry);
             mainHtml.select("body").first().append(newRow.html());
             currentRow = 0;
         }
     }
 
+    // to fit text in the box, no matter how large
+    private double getSize(String entry) {
+        if (fontSize > 0) {
+            return fontSize;
+        }
+        double size = boxHeight / entry.length();
+        if (size < 0.6)
+            size = 28 * size;
+        else
+            size = 12;
+        return size;
+    }
+
+    // helpers for append methods
+    private void setRight(String entry) {
+        String right = ".right";
+        String s = "style";
+        String style = newRow.select(right).first().attr(s);
+        style = style.replaceAll("(font-size:).*(px;)", "") + "font-size: " + getSize(entry) + "px;";
+        newRow.select(right).first().attr(s, style);
+        newRow.select(right).first().text(entry);
+    }
+
+    private void setLeft(String entry) {
+        String left = ".left";
+        String s = "style";
+        String style = newRow.select(left).first().attr(s);
+        style = style.replaceAll("(font-size:).*(px;)", "") + "font-size: " + getSize(entry) + "px;";
+        newRow.select(left).first().attr(s, style);
+        newRow.select(left).first().text(entry);
+    }
 
     String getHtml() {
         return mainHtml.html();
